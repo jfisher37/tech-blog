38273c99e9e059852b98ade0e724340d96d6ea1f
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-10-18 14:54:57 -0400

    added comment model and re-seeded data.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -11,11 +12,20 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-
+//   let postNum = -1;
   for (const post of postData) {
+    // postNum++
     await Post.create({
       ...post,
       user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      post_id: Math.floor(Math.random() * postData.length),
     });
   }
 
